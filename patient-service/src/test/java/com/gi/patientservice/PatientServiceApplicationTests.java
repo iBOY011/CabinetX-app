a9ba58b8b51a7b90c15d240e085dad5c4efa01ae
iBOY011 <147536244+iBOY011@users.noreply.github.com>
2025-12-04 17:40:39 +0100

    Remove properties that enable spring config in tests and fix patient service properties

--- patient-service/src/test/java/com/gi/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/gi/patientservice/PatientServiceApplicationTests.java
@@ -3,12 +3,7 @@ package com.gi.patientservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-        properties = {
-                "spring.cloud.config.enabled=false",
-                "spring.config.import="
-        }
-)
+@SpringBootTest
 class PatientServiceApplicationTests {
 
     @Test
