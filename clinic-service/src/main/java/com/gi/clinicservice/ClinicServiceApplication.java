a0f441ffa1fce384ef1a132e366e9799306b81d1
majidied <majidimajidi2003@gmail.com>
2025-12-19 20:27:01 +0100

    fix: Remove unused variable to comply with SonarQube rules

--- clinic-service/src/main/java/com/gi/clinicservice/ClinicServiceApplication.java
+++ clinic-service/src/main/java/com/gi/clinicservice/ClinicServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClinicServiceApplication {
 
     public static void main(String[] args) {
-        String unusedVariable = "This will trigger SonarQube unused variable rule";
         SpringApplication.run(ClinicServiceApplication.class, args);
     }
 
