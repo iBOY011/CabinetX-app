314ae02f860f90a495ed800037768af140c254ad
majidied <majidimajidi2003@gmail.com>
2025-12-19 20:09:56 +0100

    test: Add unused variable to trigger SonarQube rule S1481

--- clinic-service/src/main/java/com/gi/clinicservice/ClinicServiceApplication.java
+++ clinic-service/src/main/java/com/gi/clinicservice/ClinicServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClinicServiceApplication {
 
     public static void main(String[] args) {
+        String unusedVariable = "This will trigger SonarQube unused variable rule";
         SpringApplication.run(ClinicServiceApplication.class, args);
     }
 
