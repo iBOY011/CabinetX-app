477f603ab73093536875a1522501268e7178a9e2
majidied <majidimajidi2003@gmail.com>
2026-01-09 00:37:59 +0100

    feat: Integrate Feign clients in ConsultationServiceApplication, add method to UserService for fetching users by cabinet ID and role, and implement the corresponding logic in UserServiceImpl

--- consultation-service/src/main/java/com/gi/consultationservice/ConsultationServiceApplication.java
+++ consultation-service/src/main/java/com/gi/consultationservice/ConsultationServiceApplication.java
@@ -6,12 +6,14 @@ import com.gi.consultationservice.repository.ConsultationRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ConsultationServiceApplication {
 
     public static void main(String[] args) {
